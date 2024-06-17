cc33481e65416ec39ffc04c33ecc0a01b222b0c2
psmall06 <prs0693@yahoo.com>
2024-06-16 23:46:19 -0400

    added javascript to search engine
    entered ids into the form search

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function handleSearchSubmit(Event) {
+  Event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
