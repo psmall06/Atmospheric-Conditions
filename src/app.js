96a38770787af85a1551faea81bca8b9e0409739
psmall06 <prs0693@yahoo.com>
2024-06-17 01:22:21 -0400

    updated humidity and windspeed in JS and HTML

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ function refreshWeatherData(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
 
-  console.log(response.data.condition.description);
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
