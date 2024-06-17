ee7ea54af38a122f1798f0be625332118a0bdb20
psmall06 <prs0693@yahoo.com>
2024-06-16 23:57:55 -0400

    created a new function called searchCity
    entered information for axios, weather api key and url included in HTML and JS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = d3o08645bfa7aet464f60cce518af4bc;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key};`;
+}
+
 function handleSearchSubmit(Event) {
   Event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
