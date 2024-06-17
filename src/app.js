681f4c245dc2d05dc1a3f9f8e22826924ba00d62
psmall06 <prs0693@yahoo.com>
2024-06-17 02:43:39 -0400

    Created a div id for icon
    Updated icon image for weather data in css and js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,11 @@ function refreshWeatherData(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
+  let iconElement = document.querySelector("#icon");
+
   console.log(response.data);
 
+  iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -31,6 +34,10 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
 
   return `${day} ${hours}:${minutes}`;
 }
