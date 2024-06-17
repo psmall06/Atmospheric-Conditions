0015ea9e76bfb4241253d38fa3a4dd2157e7517d
psmall06 <prs0693@yahoo.com>
2024-06-17 00:28:37 -0400

    updated search functionality using weather api
    adjusted temperature metrics and city in search

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
+function refreshWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
-  let apiKey = d3o08645bfa7aet464f60cce518af4bc;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key};`;
+  let apiKey = "d3o08645bfa7aet464f60cce518af4bc";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeatherData);
 }
 
 function handleSearchSubmit(Event) {
   Event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
